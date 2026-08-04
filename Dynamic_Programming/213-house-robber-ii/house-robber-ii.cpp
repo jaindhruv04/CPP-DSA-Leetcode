5983195518ef58c627857fa0768c9af4d4cab327
Dhruv Jain <56800250+jaindhruv04@users.noreply.github.com>
2026-08-04 21:25:09 +0530

    Time: 2 ms (8.71%) | Memory: 11.1 MB (7.68%) - LeetSync

---
+++ Dynamic_Programming/213-house-robber-ii/house-robber-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int robhelper(vector<int>& nums) {
+        if (nums.size() == 1) {
+            return nums[0];
+        }
+
+        vector<int> dp(nums.size());
+
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.size(); i++) {
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+
+        return dp[nums.size() - 1];
+    }
+    int rob(vector<int>& nums) {
+        if (nums.size() == 1) {
+            return nums[0];
+        }
+        vector<int> first(nums.begin(), nums.end() - 1);
+        vector<int> second(nums.begin() + 1, nums.end());
+
+        return max(robhelper(first), robhelper(second));
+    }
+};
